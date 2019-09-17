1a08e54dc30bbc61ff09446bf1753562343642c7
Stefano D Minervini <minervini@mac.com>
2019-09-17 13:18:23 -0500

    First Commit, project started

---
+++ TrueCoders/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TrueCoders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var michael = new People();
+            var daniel = new People();
+            var stefano = new People();
+
+            michael.Age = 32;
+            michael.FirstName = "Michael";
+            michael.LastName = "Doyle";
+            michael.HairColor = "Brown";
+
+            daniel.Age = 26;
+            daniel.FirstName = "Daniel";
+            daniel.LastName = "Walsh";
+            daniel.HairColor = "Brownish";
+
+            stefano.Age = 55;
+            stefano.FirstName = "Stefano";
+            stefano.LastName = "Minervini";
+            stefano.HairColor = "Dark Brown";
+        }
+    }
+}
